9f74927d15b99901a6ce1976fc29fb66891fe411
GO-Shoderu <go.shoderu@gmail.com>
2023-08-19 17:23:00 -0700

    Adding the implementation of the prototype in the header file

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdlib.h>
 
 /**
  * _printf - entry level to variadic function
@@ -13,6 +14,11 @@ int _printf(const char *format, ...)
 	int number_character = 0;
 	const char *p;
 	const char *str;
+	int num;
+	int num_digits;
+	int temp;
+	int i;
+	char *digits;
 
 	va_start(args, format);
 
@@ -34,13 +40,55 @@ int _printf(const char *format, ...)
 					}
 				}
 				continue;
+
 				case 'c':
 				_putchar((char)va_arg(args, int));
 				number_character++;
 				continue;
+
 				case '%':
 				_putchar('%');
 				number_character++;
+				continue;
+
+				case 'i':
+				case 'd':
+				{
+					num = va_arg(args, int);
+
+					if (num < 0)
+					{
+						_putchar('-');
+						num = -num;
+						number_character++;
+					}
+
+					num_digits = 0;
+					temp = num;
+
+					do {
+						temp /= 10;
+						num_digits++;
+					} while (temp > 0);
+
+					digits = (char *)malloc(num_digits + 1);
+
+					for (i = num_digits - 1; i >= 0; i--)
+					{
+						digits[i] = num % 10 + '0';
+						num /= 10;
+					}
+
+					digits[num_digits] = '\0';
+
+					for (i = 0; i < num_digits; i++)
+					{
+						_putchar(digits[i]);
+						number_character++;
+					}
+
+					free(digits);
+				}
 				continue;
 			}
 		}
