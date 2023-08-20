af812e266b21a9f5f66706ade379d101d46aad84
94048 <tshepiso.matli96@gmail.com>
2023-08-20 12:37:02 -0700

    all changes

--- _printf.c
+++ _printf.c
@@ -3,9 +3,59 @@
 #include "main.h"
 
 /**
+ * _print_char - Print a character
+ * @c: Character to be printed
+ *
+ * Description: This function prints a single character to stdout.
+ *
+ * Return: Number of characters printed
+ */
+int _print_char(char c)
+{
+write(1, &c, 1);
+return (1);
+}
+
+/**
+ * _print_string - Print a string
+ * @str: String to be printed
+ *
+ * Description: This function prints a string to stdout.
+ *
+ * Return: Number of characters printed
+ */
+int _print_string(char *str)
+{
+int i = 0;
+while (str && str[i])
+{
+write(1, &str[i], 1);
+i++;
+}
+return (i);
+}
+
+/**
+ * _print_percent - Print a percent symbol
+ *
+ * Description: This function prints a percent symbol to stdout.
+ *
+ * Return: Number of characters printed
+ */
+int _print_percent(void)
+{
+write(1, "%", 1);
+return (1);
+}
+
+/**
  * _printf - Custom printf function
  * @format: Format string
  * @...: Variable arguments
+ *
+ * Description: Function processes the format string and prints output
+ * to stdout according to the specified format specifiers.
+ *
  * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
@@ -30,25 +80,15 @@ break;
 
 if (format[i] == 'c')
 {
-char c = va_arg(args, int);
-write(1, &c, 1);
-count++;
+count += _print_char(va_arg(args, int));
 }
 else if (format[i] == 's')
 {
-char *str = va_arg(args, char *);
-int j = 0;
-while (str && str[j])
-{
-write(1, &str[j], 1);
-count++;
-j++;
-}
+count += _print_string(va_arg(args, char *));
 }
 else if (format[i] == '%')
 {
-write(1, &format[i], 1);
-count++;
+count += _print_percent();
 }
 }
 i++;
