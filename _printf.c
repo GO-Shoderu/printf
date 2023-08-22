cd78a88c85bb0ce85ab3bde787cc5c004c10cb92
94048 <tshepiso.matli96@gmail.com>
2023-08-22 03:31:56 -0700

    all changes

--- _printf.c
+++ _printf.c
@@ -1,62 +1,67 @@
-#include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
 
 /**
- * _print_char - Print a character
- * @types: va_list containing arguments
- * @buffer: Character buffer
- * @flags: Formatting flags
- * @width: Formatting width
- * @precision: Formatting precision
- * @size: Size parameter (unused)
- *
- * This function prints a single character to stdout.
- *
- * Return: Number of characters printed
+ * print_char - Print a character
+ * @args: List of arguments
+ * @count: Pointer to count of printed characters
  */
-int _print_char(va_list types, char buffer[], int flags, int width,
-int precision, int size)
+void print_char(va_list args, int *count)
 {
-char c = va_arg(types, int);
-(void)buffer;
-(void)flags;
-(void)width;
-(void)precision;
-(void)size;
-
-write(1, &c, 1);
-return (1);
+char c = va_arg(args, int);
+*count += _putchar(c);
 }
 
 /**
- * _print_string - Print a string
- * @types: va_list containing arguments
- * @buffer: Character buffer
- * @flags: Formatting flags
- * @width: Formatting width
- * @precision: Formatting precision
- * @size: Size parameter (unused)
- *
- * This function prints a string to stdout.
- *
- * Return: Number of characters printed
+ * print_string - Print a string
+ * @args: List of arguments
+ * @count: Pointer to count of printed characters
  */
-int _print_string(va_list types, char buffer[], int flags, int width,
-int precision, int size)
+void print_string(va_list args, int *count)
 {
-char *str = va_arg(types, char *);
-int i = 0;
-(void)buffer;
-(void)flags;
-(void)width;
-(void)precision;
-(void)size;
-
-while (str && str[i])
+char *str = va_arg(args, char*);
+while (*str)
 {
-write(1, &str[i], 1);
-i++;
+*count += _putchar(*str);
+str++;
 }
-return (i);
+}
+
+/**
+ * _printf - produces output according to a format
+ * @format: The format string
+ *
+ * Return: Number of characters printed (excluding null byte for strings)
+ */
+int _printf(const char *format, ...)
+{
+va_list args;
+const char *p;
+int count = 0;
+
+va_start(args, format);
+p = format;
+
+while (*p)
+{
+if (*p == '%')
+{
+p++;
+if (*p == 'c')
+print_char(args, &count);
+else if (*p == 's')
+print_string(args, &count);
+else if (*p == '%')
+count += _putchar('%');
+}
+else
+{
+count += _putchar(*p);
+}
+
+p++;
+}
+
+va_end(args);
+return (count);
 }
