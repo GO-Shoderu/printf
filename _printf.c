10640934bb88b80eed176cf4d8ebcf000227e636
94048 <tshepiso.matli96@gmail.com>
2023-08-22 02:07:12 -0700

    all changes

--- _printf.c
+++ _printf.c
@@ -4,96 +4,59 @@
 
 /**
  * _print_char - Print a character
- * @c: Character to be printed
+ * @types: va_list containing arguments
+ * @buffer: Character buffer
+ * @flags: Formatting flags
+ * @width: Formatting width
+ * @precision: Formatting precision
+ * @size: Size parameter (unused)
  *
- * Description: This function prints a single character to stdout.
+ * This function prints a single character to stdout.
  *
  * Return: Number of characters printed
  */
-int _print_char(char c)
+int _print_char(va_list types, char buffer[], int flags, int width,
+int precision, int size)
 {
+char c = va_arg(types, int);
+(void)buffer;
+(void)flags;
+(void)width;
+(void)precision;
+(void)size;
+
 write(1, &c, 1);
 return (1);
 }
 
 /**
  * _print_string - Print a string
- * @str: String to be printed
+ * @types: va_list containing arguments
+ * @buffer: Character buffer
+ * @flags: Formatting flags
+ * @width: Formatting width
+ * @precision: Formatting precision
+ * @size: Size parameter (unused)
  *
- * Description: This function prints a string to stdout.
+ * This function prints a string to stdout.
  *
  * Return: Number of characters printed
  */
-int _print_string(char *str)
+int _print_string(va_list types, char buffer[], int flags, int width,
+int precision, int size)
 {
+char *str = va_arg(types, char *);
 int i = 0;
+(void)buffer;
+(void)flags;
+(void)width;
+(void)precision;
+(void)size;
+
 while (str && str[i])
 {
 write(1, &str[i], 1);
 i++;
 }
 return (i);
-}
-
-/**
- * _print_percent - Print a percent symbol
- *
- * Description: This function prints a percent symbol to stdout.
- *
- * Return: Number of characters printed
- */
-int _print_percent(void)
-{
-write(1, "%", 1);
-return (1);
-}
-
-/**
- * _printf - Custom printf function
- * @format: Format string
- * @...: Variable arguments
- *
- * Description: Function processes the format string and prints output
- * to stdout according to the specified format specifiers.
- *
- * Return: Number of characters printed
- */
-int _printf(const char *format, ...)
-{
-va_list args;
-int i = 0, count = 0;
-
-va_start(args, format);
-
-while (format && format[i])
-{
-if (format[i] != '%')
-{
-write(1, &format[i], 1);
-count++;
-}
-else
-{
-i++;
-if (format[i] == '\0')
-break;
-
-if (format[i] == 'c')
-{
-count += _print_char(va_arg(args, int));
-}
-else if (format[i] == 's')
-{
-count += _print_string(va_arg(args, char *));
-}
-else if (format[i] == '%')
-{
-count += _print_percent();
-}
-}
-i++;
-}
-
-va_end(args);
-return (count);
 }
--- main.h
+++ main.h
@@ -2,7 +2,67 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSSED(X) (void)(x)
+#define buff_size 1024
+
+/*flags*/
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/*SIZES*/
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct fmt - struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+char fmt;
+int (*fn)(va_list, char[], int, int, int, int);
+};
+
+/**
+ * typedef struct fmt fmt_t - struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
+int print_char(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_hex(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_HEX(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_custom(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_reversed(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_rot13(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
